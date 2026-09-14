c0a07390344f069a796df8b11218a3c1b594900a
carsonderda <cderda@me.com>
2026-09-14 16:25:36 +1000

    tsx

--- app/DevClickTo.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-
-import { ClickToComponent } from 'click-to-react-component'
-
-export default function DevClickTo() {
-  return <ClickToComponent />
-}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter, Playfair_Display } from "next/font/google";
 import "katex/dist/katex.min.css";
 import "./globals.css";
-import DevClickTo from './DevClickTo';
  
 const inter = Inter({ variable: "--font-inter", subsets: ["latin"] });
 const playfair = Playfair_Display({ variable: "--font-playfair", subsets: ["latin"], weight: ["400", "500", "600"], style: ["normal", "italic"] });
@@ -16,7 +15,6 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html lang="en" className={`${inter.variable} ${playfair.variable} h-full`}>
       <body className="min-h-full">
-        <DevClickTo />
         {children}
       </body>
     </html>
